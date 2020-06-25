3820ed9fec13af62973b519f19d71cb0ccc5be47
hanyao <thy495380309@gmail.com>
2020-06-25 17:29:43 +0800

    index.html
    +index.html

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo "Hello World!";
+echo "<a href='http://www.enterprisedb.com'><img src='images/edblogo.png' border=0></a></br></br>";
+echo "<b></p>Apache - version 2.4.23 </br> PHP - version 5.5.38 </br></b> Server is up and running </br> The default Apache context is <i><u>www</i></u> in the Apache installation folder</br>";
+
+?>
